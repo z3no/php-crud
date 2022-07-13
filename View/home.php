d088eee6979f07fe8c3bae5eb241cf1f063667d7
MichaelMontei <code-me@hotmail.com>
2022-07-13 14:42:58 +0200

    datasource.php aangemaakt en homecontroller.php aangemaakt

---
+++ View/home.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+include 'Views/include/header.php';
+?>
+
+
+//put View divs inside here to show the homepage
+
+
+
+
+
+<?php
+include 'Views/include/footer.php';
+?>
