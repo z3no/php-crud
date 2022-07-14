666c2822d6acf31797e1bf4627d1e52bdf058804
Zeno Driesen <zeno.driesen@gmail.com>
2022-07-14 14:26:41 +0200

    using the homepagecontroller to test creating a new datasource, retrieving group data and var dumped it, now in comments

--- Controller/HomepageController.php
+++ Controller/HomepageController.php
@@ -1,22 +1,33 @@
 <?php
 
-class HomeController {
+class HomepageController {
 
     public function render($GET, $POST){
 
         $dataSource = new DataSource();
+        $dataSource->connect();
+        $groupData = $dataSource->retrieveGroups();
+        $row = $groupData;
+
+
+        /*
+        $groupID = array_column($groupData, 'id');
+        $groupName = array_column($groupData, 'name');
+        $groupTeacher = array_column($groupData, 'teacher_id');
+        $groupCampus = array_column($groupData, 'campus_id');
+        var_dump($groupID);
+        var_dump($groupName);
+        var_dump($groupTeacher);
+        var_dump($groupCampus);
+        */
+
 
         if(isset($POST['submit'])){
 
 
-            if($POST['customerId'] !== 'Customer' && $POST['productId'] !== 'Product' && $POST['quantity'] !== 'Quantity'){
-
-
-            }
-
         }
 
-        require 'View/home.php';
+        require 'View/groupview.php';
 
     }
 }
