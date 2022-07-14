c42a98e17761e84419a5ae12cabc23fffddd3e95
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-07-14 16:27:09 +0200

    added a table in the view and made a foreach with the getters from class campus

--- View/campusview.php
+++ View/campusview.php
@@ -5,3 +5,22 @@ declare(strict_types=1);
 <section>
     <p><a href="index.php">To home page</a></p>
 </section>
+<table>
+    <thead>
+    <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Location ID</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach($showAllCampus as $campus): ?>
+        <tr>
+            <td><?php echo $campus->getId(); ?></td>
+            <td><?php echo $campus->getName(); ?></td>
+            <td><?php echo $campus->getLocation(); ?></td>
+        </tr>
+    <?php endforeach; ?>
+    </tbody>
+
+</table>
