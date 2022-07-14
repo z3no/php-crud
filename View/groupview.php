ac44001f3520325ea57b71ba64bf8f4241888394
Zeno Driesen <zeno.driesen@gmail.com>
2022-07-14 14:27:56 +0200

    trying to get data in table

--- View/groupview.php
+++ View/groupview.php
@@ -1,1 +1,33 @@
 <?php
+declare(strict_types=1);
+require 'View/includes/header.php';
+?>
+
+<div id="container">
+
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Teacher ID</th>
+                <th>Campus ID</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($groupID as $id): ?>
+            <tr>
+                <td><?php echo $id;?></td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+
+    </table>
+
+</div>
+
+
+
+<?php
+require 'View/includes/footer.php';
+?>
