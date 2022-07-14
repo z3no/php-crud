a95c4af5a415ecdafb42052db903c7e093650632
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-07-14 14:29:20 +0200

    Added render function to controller, made a connection with the datasource, display data in data source and require campus view

--- Controller/CampusController.php
+++ Controller/CampusController.php
@@ -1,6 +1,17 @@
 <?php
 
+
+
 class CampusController
 {
+    public function render($GET, $POST)
+    {
+        $dataSource = new DataSource();
+        $dbc = $dataSource ->connect();
 
+        $showAllCampus = $dataSource->getCampus();
+        var_dump($showAllCampus);
+        require 'View/campusview.php';
+    }
 }
+
