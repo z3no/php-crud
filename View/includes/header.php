a674f36941b12ce20085860aa57d4b26c4f44eba
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-07-15 09:33:03 +0200

    added bootstrap link and favicon

--- View/includes/header.php
+++ View/includes/header.php
@@ -5,7 +5,9 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Becode - Boiler plate MVC</title>
+    <title>Dragon Ball Z World</title>
+    <link rel="shortcut icon" type="image/jpg" href="https://i.pinimg.com/originals/41/d9/2b/41d92bfeba7936768e399c305b60736a.png"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
     <header>
