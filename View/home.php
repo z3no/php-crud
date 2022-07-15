96216578463d42a559ffdeccb18a9aeeb3508cfe
Zeno Driesen <zeno.driesen@gmail.com>
2022-07-15 12:08:30 +0200

    added links to go to other pages

--- View/home.php
+++ View/home.php
@@ -10,7 +10,7 @@ require 'View/includes/header.php';
         <div class="card-body">
             <h5 class="card-title">Checkout our students</h5>
             <p class="card-text">Our students are like our children, they are all equal and eager to learn.</p>
-            <a href="#" class="btn btn-primary stretched-link">Go to students</a>
+            <a href="index.php?page=student" class="btn btn-primary stretched-link">Go to students</a>
         </div>
     </div>
     <div class="card" style="width: 18rem;">
@@ -18,7 +18,7 @@ require 'View/includes/header.php';
         <div class="card-body">
             <h5 class="card-title">Our wise teachers</h5>
             <p class="card-text">These teachers have all the knowledge, wisdom and skills you'll need.</p>
-            <a href="#" class="btn btn-primary stretched-link">Go to teachers</a>
+            <a href="index.php?page=teacher" class="btn btn-primary stretched-link">Go to teachers</a>
         </div>
     </div>
     <div class="card" style="width: 18rem;">
@@ -26,7 +26,7 @@ require 'View/includes/header.php';
         <div class="card-body">
             <h5 class="card-title">See our groups</h5>
             <p class="card-text">Take a look at our awesome groups you could end up in and where their locations are.</p>
-            <a href="#" class="btn btn-primary stretched-link">Go to groups</a>
+            <a href="index.php?page=group" class="btn btn-primary stretched-link">Go to groups</a>
         </div>
     </div>
     <div class="card" style="width: 18rem;">
@@ -34,7 +34,7 @@ require 'View/includes/header.php';
         <div class="card-body">
             <h5 class="card-title">Take a look at our campuses</h5>
             <p class="card-text">Find out which campuses we have and where they are located. Click the button for more details.</p>
-            <a href="#" class="btn btn-primary stretched-link">Go to campuses</a>
+            <a href="index.php?page=campus" class="btn btn-primary stretched-link">Go to campuses</a>
         </div>
     </div>
 
