e25afbe0436aa404dc7230a54fb169219c4cc017
Zeno Driesen <zeno.driesen@gmail.com>
2022-07-15 16:54:37 +0200

    created a view for the edit group page still some things to do, my dropdown lists show the teacher and schools multiple times, will have to write another function with new query

--- View/edit_group.php
+++ View/edit_group.php
@@ -9,10 +9,27 @@ require 'View/includes/header.php';
 
         <a href="index.php?page=groups"><button type="button" class="btn btn-success">View</button></a>
 
-        <form action="">
-            <label for="name">Group name: </label>
-            <input >
+        <form action="index.php?page=groups" method="post">
+            <label for="groupname"> Group name:</label><br>
+            <input type="text" id="groupname" name="groupname" value="Change the group name"><br>
+            <label for="teacher">Choose a teacher:</label><br>
+            <select id="teacher" name="teacher">
+                <?php
+                foreach ($groupData as $data){
+                    echo "<option value='". $data->getTeacherID() . "'>" . $data->getTeacherName() . "</option>";
+                }
+                ?>
+            </select><br>
+            <label for="campus">Choose a campus:</label><br>
+            <select id="campus" name="campus">
+                <?php
+                foreach ($groupData as $data){
+                    echo "<option value='". $data->getCampusID() . "'>" . $data->getCampusName() . "</option>";
+                }
+                ?>
+            </select><br><br>
 
+            <input type="submit" name="submit" value="Submit">
         </form>
 
     </div>
