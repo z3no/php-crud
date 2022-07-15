cad12b424b8c7c98807bff592c81ee5e72d2c34a
Zeno Driesen <zeno.driesen@gmail.com>
2022-07-15 11:44:13 +0200

    created 4 cards one for students, teachers, groups and campuses

--- View/home.php
+++ View/home.php
@@ -4,6 +4,41 @@ require 'View/includes/header.php';
 ?>
 
 
+<section class="d-flex">
+    <div class="card" style="width: 18rem;">
+        <img src="./images/students.jpeg" class="card-img-top" alt="students image">
+        <div class="card-body">
+            <h5 class="card-title">Checkout our students</h5>
+            <p class="card-text">Our students are like our children, they are all equal and eager to learn.</p>
+            <a href="#" class="btn btn-primary stretched-link">Go to students</a>
+        </div>
+    </div>
+    <div class="card" style="width: 18rem;">
+        <img src="./images/masterRoshi.jpg" class="card-img-top" alt="teacher image">
+        <div class="card-body">
+            <h5 class="card-title">Our wise teachers</h5>
+            <p class="card-text">These teachers have all the knowledge, wisdom and skills you'll need.</p>
+            <a href="#" class="btn btn-primary stretched-link">Go to teachers</a>
+        </div>
+    </div>
+    <div class="card" style="width: 18rem;">
+        <img src="./images/dragonTeam.jpeg" class="card-img-top" alt="group image">
+        <div class="card-body">
+            <h5 class="card-title">See our groups</h5>
+            <p class="card-text">Take a look at our awesome groups you could end up in and where their locations are.</p>
+            <a href="#" class="btn btn-primary stretched-link">Go to groups</a>
+        </div>
+    </div>
+    <div class="card" style="width: 18rem;">
+        <img src="./images/kamiSamaTemple.png" class="card-img-top" alt="campus image">
+        <div class="card-body">
+            <h5 class="card-title">Take a look at our campuses</h5>
+            <p class="card-text">Find out which campuses we have and where they are located. Click the button for more details.</p>
+            <a href="#" class="btn btn-primary stretched-link">Go to campuses</a>
+        </div>
+    </div>
+
+</section>
 
 
 
