49f423437280fcc903d16d9876fb4cef61dd5b5f
Zeno Driesen <zeno.driesen@gmail.com>
2022-07-15 15:36:13 +0200

    added  variable created a variable so it will echo a title, this can then be changed depending on what page we are so we don't need to make different headers because the title can be different

--- View/includes/header.php
+++ View/includes/header.php
@@ -5,11 +5,11 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Dragon Ball Z World</title>
+    <title><?php echo $page_title; ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css"/>
 </head>
 <body>
     <header>
-        <h1>Welcome to our groups page</h1>
+        <?php echo "<h1>$page_title</h1>"?>
     </header>
