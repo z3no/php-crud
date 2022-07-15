eb2bd6848d9aca26a63f4d21055d21ca2953b295
Zeno Driesen <zeno.driesen@gmail.com>
2022-07-15 15:34:24 +0200

    edit group view is born

---
+++ View/edit_group.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+$page_title = "Edit group";
+require 'View/includes/header.php';
+?>
+
+    <div id="container">
+
+        <a href="index.php?page=groups"><button type="button" class="btn btn-success">View</button></a>
+
+        <form action="">
+            <label for="name">Group name: </label>
+            <input >
+
+        </form>
+
+    </div>
+
+
+
+<?php
+require 'View/includes/footer.php';
+?>
