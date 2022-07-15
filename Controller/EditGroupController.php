acb1e33c4bc8c944c5f02294cf3eae8bd7c9ffaa
Zeno Driesen <zeno.driesen@gmail.com>
2022-07-15 15:33:57 +0200

    edit group controller is born

---
+++ Controller/EditGroupController.php
@@ -0,0 +1,16 @@
+<?php
+
+class EditGroupController
+{
+    public function render($GET, $POST){
+
+
+        if(isset($POST['submit'])){
+
+
+        }
+
+        require 'View/edit_group.php';
+
+    }
+}
