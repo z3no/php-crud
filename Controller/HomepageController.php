6aa3bcf0e25b53b35e810f95f5605a11e0e56fbc
MichaelMontei <code-me@hotmail.com>
2022-07-16 20:37:17 +0200

    everything works again

--- Controller/HomepageController.php
+++ Controller/HomepageController.php
@@ -17,27 +17,6 @@ class HomepageController {
         $test = $dataSource->collectAllTeachers();
         $showAllCampus = $dataSource->getCampus();
 
-
-        //$showAllTeachers = $dataSource->getTeachers();
-
-        //dump($showAllTeachers);
-
-
-        /*
-        $groupID = array_column($groupData, 'id');
-        $groupName = array_column($groupData, 'name');
-        $groupTeacher = array_column($groupData, 'teacher_id');
-        $groupCampus = array_column($groupData, 'campus_id');
-        var_dump($groupID);
-        var_dump($groupName);
-        var_dump($groupTeacher);
-        var_dump($groupCampus);
-        */
-
-        if(isset($POST['submit'])) {
-
-
-        }
         require 'View/home.php';
     }
 }
