58380603a1f5ef2a539eb1b1db511c17ca234da0
Zeno Driesen <zeno.driesen@gmail.com>
2022-07-18 10:08:09 +0200

    centered footer and pushed down on page

--- View/includes/footer.php
+++ View/includes/footer.php
@@ -1,5 +1,5 @@
-    <footer>
-        &copy; BeCode <?php echo date('Y')?>
+    <footer class="d-flex justify-content-center mt-auto">
+       <p> &copy; BeCode <?php echo date('Y')?> || by Michael, Pablo, Alex & Zeno</p>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
     </body>
