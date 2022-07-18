83f1ed72c1d4a81bf0d00acd5f50e483b8e640ed
Zeno Driesen <zeno.driesen@gmail.com>
2022-07-18 10:02:58 +0200

    centered header title

--- View/includes/header.php
+++ View/includes/header.php
@@ -11,7 +11,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.0/font/bootstrap-icons.css">
 </head>
-<body class="">
-    <header>
+<body>
+    <header class="d-flex justify-content-center my-5">
         <?php echo "<h1>$page_title</h1>"; ?>
     </header>
