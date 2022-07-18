16504753f3f274a2742cb51833d27da8b34eb7b6
Zeno Driesen <zeno.driesen@gmail.com>
2022-07-18 09:41:26 +0200

    changed title

--- View/includes/header.php
+++ View/includes/header.php
@@ -6,14 +6,11 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <title>Dragon Ball Z - Groups</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-
-    <title>Dragon Ball Z World</title>
+    <title><?php echo $page_title; ?></title>
     <link rel="shortcut icon" type="image/jpg" href="https://i.pinimg.com/originals/41/d9/2b/41d92bfeba7936768e399c305b60736a.png"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
     <header>
-        <h1>Welcome to our groups page</h1>
+        <?php echo "<h1>$page_title</h1>"; ?>
     </header>
